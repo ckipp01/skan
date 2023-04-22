d2ceedf1c5adb2cd63182f08e2e17caa6b64d080
Chris Kipp <ckipp@pm.me>
2023-04-22 10:58:52 +0200

    refactor: try with a prepare release script

---
+++ .github/prepare-release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p releases/
+
+CWD=$(pwd -P)
+            
+chmod +x skan-x86_64-apple-darwin/skan
+tar cvfz "$CWD/releases/skan-x86_64-apple-darwin.tar.gz" skan-x86_64-apple-darwin/skan
+
+chmod +x skan-x86_64-linux/skan
+tar cvfz "$CWD/releases/skan-x86_64-linux.tar.gz" skan-x86_64-linux/skan)
