de4294d1c56f6ff10733cf788bcd9a5cf1444237
Chris Kipp <ckipp@pm.me>
2023-04-22 11:12:08 +0200

    fix: yet another typo

--- .github/prepare-release.sh
+++ .github/prepare-release.sh
@@ -10,4 +10,4 @@ chmod +x skan-x86_64-apple-darwin/skan
 tar cvfz "$CWD/releases/skan-x86_64-apple-darwin.tar.gz" skan-x86_64-apple-darwin/skan
 
 chmod +x skan-x86_64-linux/skan
-tar cvfz "$CWD/releases/skan-x86_64-linux.tar.gz" skan-x86_64-linux/skan)
+tar cvfz "$CWD/releases/skan-x86_64-linux.tar.gz" skan-x86_64-linux/skan
